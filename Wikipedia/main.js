3d1ac8f64cd8df748b190d64a76f776b028b9170
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2019-08-12 17:53:07 -0300

    criando elementos pra pesquisa

---
+++ Wikipedia/main.js
@@ -0,0 +1,228 @@
+function renderMain(){
+	const api = {
+	"batchcomplete":"",
+	"continue":
+		{
+			"sroffset":20,
+			"continue":"-||info"
+		},
+		"query":
+			{
+				"searchinfo":	{
+						"totalhits":26791
+				},
+				"search":
+					[
+						{
+							"ns":0,
+							"title":"Belgrade",
+							"pageid":55904,
+							"size":157101,
+							"wordcount":13809,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> (/ˈbɛlɡreɪd/ BEL-grayd; Serbian: Beograd / Београд, lit. 'White City', pronounced [beǒɡrad] (listen); names in other languages) is the capital",
+							"timestamp":"2019-08-05T16:26:08Z"
+						},
+						{
+							"ns":0,
+							"title":"FK Partizan",
+							"pageid":381658,
+							"size":129767,
+							"wordcount":10565,
+							"snippet":"referred to as Partizan <span class=\"searchmatch\">Belgrade</span> outside of Serbia, or simply as Partizan, is a Serbian professional football club based in <span class=\"searchmatch\">Belgrade</span>. It forms a major part",
+							"timestamp":"2019-08-07T11:55:53Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade Nikola Tesla Airport",
+							"pageid":1467219,
+							"size":44006,
+							"wordcount":2584,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> Nikola Tesla Airport (Serbian: Аеродром Никола Тесла Београд / Aerodrom Nikola Tesla Beograd) or <span class=\"searchmatch\">Belgrade</span> Airport (Serbian: Аеродром Београд /",
+							"timestamp":"2019-08-08T06:28:16Z"
+						},
+						{	
+							"ns":0,
+							"title":"Red Star Belgrade",
+							"pageid":361966,
+							"size":95550,
+							"wordcount":6497,
+							"snippet":"Star <span class=\"searchmatch\">Belgrade</span> (Serbian: Црвена звезда Београд / Crvena zvezda Beograd) or simply Red Star, is a Serbian professional football club based in <span class=\"searchmatch\">Belgrade</span>, the",
+							"timestamp":"2019-08-07T08:46:47Z"
+						},
+						{
+							"ns":0,
+							"title":"Serbia",
+							"pageid":29265,
+							"size":228836,
+							"wordcount":21377,
+							"snippet":"approximately seven million, most of whom are Orthodox Christians. Its capital, <span class=\"searchmatch\">Belgrade</span>, ranks among the oldest and largest citiеs in southeastern Europe. Inhabited",	
+							"timestamp":"2019-08-05T17:43:14Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade (disambiguation)",
+							"pageid":87324,
+							"size":1374,
+							"wordcount":149,
+							"snippet":"Grižane-Belgrad <span class=\"searchmatch\">Belgrade</span>, Maine <span class=\"searchmatch\">Belgrade</span>, Minnesota <span class=\"searchmatch\">Belgrade</span>, Missouri <span class=\"searchmatch\">Belgrade</span>, Montana <span class=\"searchmatch\">Belgrade</span>, Nebraska <span class=\"searchmatch\">Belgrade</span>, Texas <span class=\"searchmatch\">Belgrade</span> Township (disambiguation)",	
+							"timestamp":"2018-08-21T16:24:38Z"
+						},
+						{	
+							"ns":0,
+							"title":"Belgrade Lakes",
+							"pageid":50464189,
+							"size":9620,
+							"wordcount":878,
+							"snippet":"The <span class=\"searchmatch\">Belgrade</span> Lakes are a chain of lakes around <span class=\"searchmatch\">Belgrade</span>, Maine. The flow sequence is from East Pond to North Pond to Great Pond to Long Pond to Messalonskee",
+							"timestamp":"2017-05-27T00:32:28Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade Fortress",
+							"pageid":823148,"size":53132,
+							"wordcount":6429,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> Fortress (Serbian Cyrillic: Београдска тврђава / Serbian Latin: Beogradska tvrđava), consists of the old citadel (Upper and Lower Town) and Kalemegdan",
+							"timestamp":"2019-07-28T09:52:04Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade, Maine",
+							"pageid":116011,
+							"size":14619,
+							"wordcount":1330,
+							"snippet":"Messalonskee Lake. <span class=\"searchmatch\">Belgrade</span> includes the villages of North <span class=\"searchmatch\">Belgrade</span>, <span class=\"searchmatch\">Belgrade</span> Depot and <span class=\"searchmatch\">Belgrade</span> Lakes (or The Village). <span class=\"searchmatch\">Belgrade</span> is included in the Augusta",
+							"timestamp":"2019-07-07T04:34:02Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade–Bar railway",
+							"pageid":5394425,
+							"size":10548,
+							"wordcount":608,
+							"snippet":"The <span class=\"searchmatch\">Belgrade</span>–Bar railway (Serbian: Пруга Београд–Бар or Pruga Beograd–Bar) is a railway connecting the Serbian capital of <span class=\"searchmatch\">Belgrade</span> with the town of Bar",
+							"timestamp":"2019-07-09T06:12:13Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade, Montana",
+							"pageid":123390,
+							"size":13710,
+							"wordcount":1266,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> is a town in Gallatin County, Montana, United States. The population was 7,389 at the 2010 census. It is the largest city in Montana that is not",
+							"timestamp":"2019-07-04T20:22:34Z"
+						},
+						{
+							"ns":0,
+							"title":"KK Crvena zvezda",
+							"pageid":5604103,
+							"size":59753,
+							"wordcount":5268,
+							"snippet":"simply Crvena zvezda, is a men's professional basketball club based in <span class=\"searchmatch\">Belgrade</span>, Serbia, the major part of the Red Star multi-sports club. The club is",
+							"timestamp":"2019-08-03T11:54:05Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade Waterfront",
+							"pageid":41948369,
+							"size":11520,
+							"wordcount":912,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> Waterfront, known in Serbian as <span class=\"searchmatch\">Belgrade</span> on Water (Београд на води / Beograd na vodi), is an urban renewal development project headed by the",
+							"timestamp":"2019-07-18T18:05:34Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade Offensive",
+							"pageid":17229910,
+							"size":49579,
+							"wordcount":6211,
+							"snippet":"The <span class=\"searchmatch\">Belgrade</span> Offensive or the <span class=\"searchmatch\">Belgrade</span> Strategic Offensive Operation (Serbo-Croatian Latin: Beogradska operacija, Београдска операција; Russian: Белградская",
+							"timestamp":"2019-08-04T06:17:16Z"
+						},
+						{
+							"ns":0,
+							"title":"Slobodan Milošević",
+							"pageid":25862556,
+							"size":109833,
+							"wordcount":12933,
+							"snippet":"committed suicide in 1963. Milošević went on to study law at the University of <span class=\"searchmatch\">Belgrade</span>'s Law School, where he became the head of the ideology committee of the",
+							"timestamp":"2019-08-08T16:36:33Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade–Banja Luka",
+							"pageid":48943441,
+							"size":3474,
+							"wordcount":69,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> Banjaluka is a road bicycle race held annually in Serbia and Bosnia and Herzegovina respectively. It is made up of two stages; <span class=\"searchmatch\">Belgrade</span> Banjaluka",
+							"timestamp":"2018-09-02T15:05:51Z"
+						},
+						{
+							"ns":0,
+							"title":"University of Belgrade",
+							"pageid":918895,
+							"size":33251,
+							"wordcount":3130,
+							"snippet":"The University of <span class=\"searchmatch\">Belgrade</span> (Serbian: Универзитет у Београду, romanized: Univerzitet u Beogradu) is a public university in Serbia. It is the oldest and",
+							"timestamp":"2019-08-07T03:49:47Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade, Namur",
+							"pageid":43523032,
+							"size":1278,
+							"wordcount":115,
+							"snippet":"<span class=\"searchmatch\">Belgrade</span> (Walloon: <span class=\"searchmatch\">Belgråde</span>) is an old village (now a suburb) close to the city centre of Namur in the Walloon region of Belgium. It was named after Belgrade",
+							"timestamp":"2018-08-21T10:07:49Z"
+						},
+						{
+							"ns":0,
+							"title":"New Belgrade",
+							"pageid":993844,
+							"size":61515,
+							"wordcount":6851,
+							"snippet":"New <span class=\"searchmatch\">Belgrade</span> (Serbian: Нови Београд, romanized: Novi Beograd, lit. 'New White City', pronounced [nôʋiː beǒɡrad]) is a municipality of the city of Belgrade",
+							"timestamp":"2019-08-06T14:35:07Z"
+						},
+						{
+							"ns":0,
+							"title":"Belgrade Main railway station",
+							"pageid":34028651,
+							"size":30615,
+							"wordcount":3480,
+							"snippet":"The <span class=\"searchmatch\">Belgrade</span> Main railway station (Serbian: Железничка станица Београд Главна / Železnička stanica Beograd Glavna) was a train station in <span class=\"searchmatch\">Belgrade</span>, the",
+							"timestamp":"2019-07-01T18:57:04Z"
+						}
+					]
+			}
+	}
+	// console.log(api.query.search)
+	let search = api.query.search
+	console.log(search)
+
+	function creatEl(elemento, texto){
+		let el = document.createElement(elemento);
+		let txt = document.createTextNode(texto)
+
+		let elTxt = el.appendChild(txt)
+		return elTxt
+	}
+	function creatTag(elemento){
+		let el = document.createElement(elemento);
+		return el
+	}
+	function styleTag(){
+		
+	}
+
+	
+	let tag = creatTag('form')
+	let imputEntrada = creatTag('imput')
+
+	imputEntrada.setAttribute('type', 'submit')
+	
+	tag.appendChild(imputEntrada)
+	document.body.appendChild(tag)
+	tag.style.height = '45vh'
+}
+document.window = renderMain()
+
