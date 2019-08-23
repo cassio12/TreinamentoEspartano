ca5d68aaf8bda0f89d297bd8d4ec450109e0dc22
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2019-08-23 17:15:27 -0300

    iniciando desenho da landing page

--- Challenge-da-Conpass/hotspot/src/App.js
+++ Challenge-da-Conpass/hotspot/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="App-header_navegation">
+          <figure className="navegation_boxImg">
+            <img src={logo} className="boxImg_logo"/>
+          </figure>
+          <ul className="navegation_boxlist">
+            <li className="boxlist_itemList">Link 1</li>
+            <li className="boxlist_itemList">Link 2</li>
+            <li className="boxlist_itemList">Link 3</li>
+            <li className="boxlist_itemList">Link 4</li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
