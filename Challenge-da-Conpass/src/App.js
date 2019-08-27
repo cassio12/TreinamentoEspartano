36e5508712358d049584aadb3429da02a3ac8d1d
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2019-08-27 17:25:33 -0300

    layout continue

---
+++ Challenge-da-Conpass/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App--header">
+        <nav className="App--header_navegation">
+          <figure className="navegation_boxImg">
+            <img src={logo} className="boxImg_logo"/>
+          </figure>
+          <div className="navegation_boxlist">
+            <ul className="boxlist_ul">
+              <li className="boxlist_itemList">Link fake 1</li>
+              <li className="boxlist_itemList">Link fake 2</li>
+              <li className="boxlist_itemList">Link fake 3</li>
+              <li className="boxlist_itemList">Link fake 4</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main className="App--main">
+        <section className="main_sectionSendItens">
+          <div className="sectionSendItens_box--centralization">
+            <div className="box--centralization_boxBtn">
+              <button className="boxBtn_btn">Create Hotspost</button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
